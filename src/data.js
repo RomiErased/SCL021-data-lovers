b434c4e5a8814b797e384ba2d901273c066f50d4
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2022-07-29 10:45:10 -0400

    cambios js

--- src/data.js
+++ src/data.js
@@ -1,16 +1,7 @@
-export const fetchPokemon = () => {
-  const url = `https://github.com/Macamescobar/SCL021-data-lovers/blob/main/src/data/pokemon/pokemon.json/001`;
-  fetch (url)
-  .then (res => {
-      return res.json();
-  })
-  .then (data => {
-      console.log(data);
-      const pokemon = {};
-      pokemon['name'] = data.name;
-      console.log(pokemon);
-  });
-}
-
-fetchPokemon();
-
+// estas funciones son de ejemplo
+//export const example = () => {
+//return 'example';
+//};
+//export const anotherExample = () => {
+//  return 'OMG';
+//};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import data from './data/pokemon/pokemon.js';
 
 
+const pokedex = document.getElementById("root");
+
+const dataPokemon = data.pokemon;
+for (let i = 0; i < dataPokemon.length; i++) {
+  // console.log(dataPokemon[i].name)
+
+  //Crear etiqueta p para nombres
+  const div = document.createElement("p");
+  div.textContent = ` ${dataPokemon[i].name} `;
+  pokedex.appendChild(div);
+  //Crear etiqueta img para agregar imagenes
+  const img = document.createElement("img")
+  img.src = `${dataPokemon[i].img}`;
+  pokedex.appendChild(img);
+}
+
+
+//console.log(dataPokemon[0]);
+//console.log(data.pokemon);
+//console.log(data);
+
+
 console.log(data);
